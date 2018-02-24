0a8533a46e808ba6029b8d7c581b7209f8a9599d
Nate Post <nate12345678nate@gmail.com>
2018-02-24 12:46:54 -0600

    what the fuck did you just call me, you little bitch?

--- src/main/java/main/GoodBoyeBot.java
+++ src/main/java/main/GoodBoyeBot.java
@@ -10,13 +10,21 @@ import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
+import java.util.LinkedHashMap;
+
 import javax.security.auth.login.LoginException;
+
+import commands.Command;
+import commands.Help;
 
 public class GoodBoyeBot extends ListenerAdapter {
 	static JDA jda;
 	static String key = Globals.KEY + "";
+	private static final LinkedHashMap<String, Command> commands = Command.getCommandMap();
+	
 	
 	public static void main(String[] args) {
+		commandToMap();
 		try {
 			jda = new JDABuilder(AccountType.BOT).setToken(Globals.DISCORD_TOKEN).buildBlocking();
 			jda.addEventListener(new GoodBoyeBot());
@@ -27,6 +35,10 @@ public class GoodBoyeBot extends ListenerAdapter {
 		}
 	}
 	
+	private static void commandToMap() {
+		Help help = new Help();
+	}
+	
 	@Override
 	public void onMessageReceived(MessageReceivedEvent event) {
 		User author = event.getAuthor();
@@ -34,6 +46,14 @@ public class GoodBoyeBot extends ListenerAdapter {
 		MessageChannel channel = event.getChannel();
 		TextChannel ch = event.getTextChannel();
 		String messageContent = message.getContentRaw();
-		if((messageContent.startsWith(key) || channel.getName().equals("bot"))&& !author.isBot()) channel.sendMessage("Hello, " + author.getName() + "!").queue();
+		if ((messageContent.startsWith(key) || channel.getName().equals("bot")) && !author.isBot()) {
+			if (messageContent.startsWith(key)) messageContent = messageContent.substring(1);
+			String[] args = messageContent.split(" ");
+			if (commands.containsKey(args[0])) {
+				commands.get(args[0]).run(event, null);
+			} else {
+				channel.sendMessage("What the fuck did you just fucking say about me, you little bitch? I’ll have you know I graduated top of my class in the Navy Seals, and I’ve been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I’m the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You’re fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that’s just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little “clever” comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn’t, you didn’t, and now you’re paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You’re fucking dead, kiddo.\n\n(That is not a command)").queue();
+			}
+		}
 	}
 }
